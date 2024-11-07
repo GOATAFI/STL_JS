36b2e59b08f6d158d9322435cabcb5126f8b9bb5
GOATAFI <projectexecutiveofpm@gmail.com>
2024-11-07 10:46:16 +0600

    string er case matching

---
+++ string_case.js
@@ -0,0 +1,8 @@
+const n1 = " Chemistry";
+const n2 = "chemistry";
+
+if (n1.trim().toLowerCase() === n2.trim().toLowerCase()) {
+  console.log("YEEEEEE");
+} else {
+  console.log("NOOOOOOOOOO");
+}
